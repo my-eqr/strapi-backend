aacd4562c3345970de608a8362a326b8287cd5f8
Muhammad-Asyraf <asyraf.rmc@gmail.com>
2024-01-21 16:06:54 +0800

    initial commit

---
+++ config/plugins.ts
@@ -0,0 +1,16 @@
+
+export default {
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
